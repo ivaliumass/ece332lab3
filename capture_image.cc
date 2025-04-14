a527f82913b8c7740dfb8cb142e734eb3d97cd63
carly-rosenstrauch <144377211+carly-rosenstrauch@users.noreply.github.com>
2025-04-14 19:38:05 -0400

    Update capture_image.cc

--- capture_image.cc
+++ capture_image.cc
@@ -12,6 +12,7 @@
 #define PUSH_BASE 0x3010
 #define VIDEO_BASE 0x0000
 
+
 #define IMAGE_WIDTH 320
 #define IMAGE_HEIGHT 240
 
@@ -20,6 +21,8 @@
 #define IMAGE_MASK (IMAGE_SPAN - 1)
 
 int main(void){
+    short exit = 1;
+    int press;
     void *virtual_base;
     void *virtual_base2;
     volatile unsigned int *video_in_dma = NULL;
@@ -27,7 +30,8 @@ int main(void){
     int fd;
     unsigned short pixels[IMAGE_WIDTH * IMAGE_HEIGHT];
     unsigned char pixels_bw[IMAGE_WIDTH * IMAGE_HEIGHT];
-    
+    const char* filename = "final_image_color.bmp";
+    const char* filename1 = "final_image_bw.bmp";
     // Open /dev/mem
     if ((fd = open("/dev/mem", (O_RDWR | O_SYNC))) == -1) {
         printf("ERROR: could not open \"/dev/mem\"...\n");
@@ -50,50 +54,58 @@ int main(void){
         close(fd);
         return 1;
     }
-
-    // Initialize pointers
+    volatile int * KEY_ptr = (int *)(virtual_base + PUSH_BASE);
     video_in_dma = (unsigned int *)(virtual_base + VIDEO_BASE);
-    video_mem = (volatile unsigned short *)(virtual_base2 +(FPGA_ONCHIP_BASE & IMAGE_MASK));// chip
-
-    // Access the video input DMA register
-    int value = *(video_in_dma + 3);
-    printf("Video In DMA register updated at: 0x%x\n", (unsigned int)video_in_dma);
+    video_mem = (volatile unsigned short *)(virtual_base2 +(FPGA_ONCHIP_BASE & IMAGE_MASK)); // chip
 
     // Modify the PIO register
     *(video_in_dma + 3) = 0x4;
-    printf("DB1 \n");
+    printf("Video in DMA register updated at: 0x%x\n", (unsigned int)video_in_dma);
+    while(exit){
+        // Initialize pointers
 
-    // Capture image
-    for (int y = 0; y < IMAGE_HEIGHT; y++) {
-        for (int x = 0; x < IMAGE_WIDTH; x++) {
-            int index = (y * IMAGE_WIDTH) + x;
-            pixels[index] = video_mem[(y<<9)+x];  // Uncommented this line
-            pixels_bw[index] = video_mem[(y<<9) +x];
+        // Access the video input DMA register
+        int value = *(video_in_dma + 3);
+
+        // iterate over image pixels
+        for (int y = 0; y < IMAGE_HEIGHT; y++) {
+            for (int x = 0; x < IMAGE_WIDTH; x++) {
+                int index = (y * IMAGE_WIDTH) + x;
+                pixels[index] = video_mem[(y<<9)+x];  // Uncommented this line
+                pixels_bw[index] = video_mem[(y<<9) +x];
+            }
+        }
+
+        // poll for button press
+        press = *(KEY_ptr);
+        *(KEY_ptr + 3) = press; // Clear press event
+
+        if(!(press & 0x1)){
+            printf("No photos taken: breaking from control \n");
+            break; // interrupt from hardware
+        }
+        else if(!(press & 0x2)){ // take images and break from control
+            saveImageShort(filename, pixels, IMAGE_WIDTH, IMAGE_HEIGHT);
+            saveImageGrayscale(filename1, pixels_bw, IMAGE_WIDTH, IMAGE_HEIGHT);
+            printf("images captured: breaking from control \n");
+            break;
+        }
+        else if(!(press & (0x4 | 0x8))){
+            printf("empty button \n");
         }
     }
 
-    printf("DB2 \n");
-    const char* filename1 = "final_image_bw.bmp";
-    // Save image
-    const char* filename = "final_image_color.bmp";
-    saveImageShort(filename, pixels, IMAGE_WIDTH, IMAGE_HEIGHT);
-    saveImageGrayscale(filename1, pixels_bw, IMAGE_WIDTH, IMAGE_HEIGHT);
-
-
-    // Cleanup - uncommented and fixed
+    // ummap memory
     if (munmap(virtual_base2, IMAGE_SPAN) != 0) {
         printf("ERROR: munmap() for FPGA memory failed...\n");
-        //munmap(virtual_base, HW_REGS_SPAN);
         close(fd);
         return 1;
     }
-
     if (munmap(virtual_base, HW_REGS_SPAN) != 0) {
         printf("ERROR: munmap() for HW registers failed...\n");
         close(fd);
         return 1;
     }
-
     close(fd);
     return 0;
 }
